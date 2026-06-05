7bf5aa69c19281e95dcbe8d3a4d6426cee096bd6
nBringuier <nicolas.bringuier@gmail.com>
2026-06-05 07:09:37 +0000

    feat: prepare scenario tree

---
+++ src/main/webui/src/app/scenario-tree/scenario-tree.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scenario-tree',
+  imports: [],
+  templateUrl: './scenario-tree.component.html',
+  styleUrl: './scenario-tree.component.css'
+})
+export class ScenarioTreeComponent {
+
+}
